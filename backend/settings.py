3342aa1140e721a00b189873bf27608e971b3782
RandyBrilliant <randybrilliant68@gmail.com>
2026-01-03 23:54:06 +0700

    Update docker-compose and settings for improved deployment
    - Removed version specification from `docker-compose.prod.yml` for simplicity.
    - Added 'django.contrib.staticfiles' to `INSTALLED_APPS` in `settings.py` to support static file management during deployment.

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',  # Required for collectstatic
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
